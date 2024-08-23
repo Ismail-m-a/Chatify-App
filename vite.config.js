792cc1bfeebef7c0b364aa63d7ef1ead03bfbe02
ismail-m-a <ismaaciilmuxamadaadam@gmail.com>
2024-08-23 18:45:25 +0200

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePluginStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePluginStaticCopy({
+      targets: [
+        {
+          src: '_redirects', // The file in the root of your project
+          dest: '.' // Copies to the root of the dist folder
+        }
+      ]
+    })
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://chatify-api.up.railway.app',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
